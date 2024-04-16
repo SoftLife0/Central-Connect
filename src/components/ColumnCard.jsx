de077aa08e266429749d27d0272ce3598f9a8e98
SoftLife0 <onikosiadewale18@gmail.com>
2024-04-16 12:26:48 +0000

    year page

--- src/components/ColumnCard.jsx
+++ src/components/ColumnCard.jsx
@@ -1,18 +1,48 @@
-import React from 'react';
-import { Grid, Paper } from '@mui/material';
+import * as React from 'react';
+import { styled, Grid, Paper } from '@mui/material';
 import { Link } from 'react-router-dom';
 
-const DashboardItem = ({ title, count, url }) => (
-  <Grid item xs={12} sm={6} md={4}>
-    <Paper className="dashboard-item" style={{ padding: '30px', marginBottom: '20px' }}>
-      <Link to={url} style={{ textDecoration: 'none', color: 'inherit' }}>
-        <div data-aos="fade-up" data-aos-delay="100" className="card" style={{ textAlign: 'center' }}>
-          <h1 style={{ fontWeight: 'bold', marginBottom: '10px' }}>{title}</h1>
-          <h4 style={{ marginBottom: '0' }}>{count}</h4>
-        </div>
-      </Link>
-    </Paper>
-  </Grid>
-);
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
-export default DashboardItem;
+const ColumnCard = ({ url, title, count }) => {
+  return (
+    <>
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid item xs={6} md={4} className='dashboard-item' style={{ backgroundColor: '#fff', padding: '30px', borderRadius: '15px', color: '#000', margin: 0, boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px', marginBottom: '5vh', justifyContent: 'space-between' }}>
+          <Link to={url} style={{ textDecoration: 'none', color: 'inherit' }}>
+            <div data-aos="fade-up" data-aos-delay="100" className="card" style={{ textAlign: 'center' }}>
+              <h1 style={{fontWeight: 'bold' }}>{title}</h1>
+              <h4>{count}</h4>
+            </div>
+          </Link>
+        </Grid>
+
+        <Grid item xs={6} md={4} className='dashboard-item' style={{ backgroundColor: '#fff', padding: '30px', borderRadius: '15px', color: '#000', margin: 0, boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px', marginBottom: '5vh', justifyContent: 'space-between' }}>
+          <Link to={url} style={{ textDecoration: 'none', color: 'inherit' }}>
+            <div data-aos="fade-up" data-aos-delay="100" className="card" style={{ textAlign: 'center' }}>
+              <h1 style={{fontWeight: 'bold' }}>{title}</h1>
+              <h4>{count}</h4>
+            </div>
+          </Link>
+        </Grid>
+
+        <Grid item xs={6} md={4} className='dashboard-item' style={{ backgroundColor: '#fff', padding: '30px', borderRadius: '15px', color: '#000', margin: 0, boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px', marginBottom: '5vh', justifyContent: 'space-between' }}>
+          <Link to={url} style={{ textDecoration: 'none', color: 'inherit' }}>
+            <div data-aos="fade-up" data-aos-delay="100" className="card" style={{ textAlign: 'center' }}>
+              <h1 style={{fontWeight: 'bold' }}>{title}</h1>
+              <h4>{count}</h4>
+            </div>
+          </Link>
+        </Grid>
+      </Grid>
+    </>
+  );
+}
+
+export default ColumnCard;
