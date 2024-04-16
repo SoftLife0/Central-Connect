87c2dc7477aad000ca0f86eda5110f534bff347b
SoftLife0 <onikosiadewale18@gmail.com>
2024-04-16 12:29:27 +0000

    year page

--- src/components/ColumnCard.jsx
+++ src/components/ColumnCard.jsx
@@ -14,7 +14,7 @@ const ColumnCard = ({ url, title, count }) => {
   return (
     <>
       <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        <Grid item xs={6} md={4} className='dashboard-item' style={{ backgroundColor: '#fff', padding: '30px', borderRadius: '15px', color: '#000', margin: 0, boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px', marginBottom: '5vh', justifyContent: 'space-between' }}>
+        <Grid item xs={6} md={6} className='dashboard-item' style={{ backgroundColor: '#fff', padding: '30px', borderRadius: '15px', color: '#000', margin: 0, boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px', marginBottom: '5vh', justifyContent: 'space-between' }}>
           <Link to={url} style={{ textDecoration: 'none', color: 'inherit' }}>
             <div data-aos="fade-up" data-aos-delay="100" className="card" style={{ textAlign: 'center' }}>
               <h1 style={{fontWeight: 'bold' }}>{title}</h1>
@@ -23,7 +23,7 @@ const ColumnCard = ({ url, title, count }) => {
           </Link>
         </Grid>
 
-        <Grid item xs={6} md={4} className='dashboard-item' style={{ backgroundColor: '#fff', padding: '30px', borderRadius: '15px', color: '#000', margin: 0, boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px', marginBottom: '5vh', justifyContent: 'space-between' }}>
+        <Grid item xs={6} md={6} className='dashboard-item' style={{ backgroundColor: '#fff', padding: '30px', borderRadius: '15px', color: '#000', margin: 0, boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px', marginBottom: '5vh', justifyContent: 'space-between' }}>
           <Link to={url} style={{ textDecoration: 'none', color: 'inherit' }}>
             <div data-aos="fade-up" data-aos-delay="100" className="card" style={{ textAlign: 'center' }}>
               <h1 style={{fontWeight: 'bold' }}>{title}</h1>
@@ -32,14 +32,7 @@ const ColumnCard = ({ url, title, count }) => {
           </Link>
         </Grid>
 
-        <Grid item xs={6} md={4} className='dashboard-item' style={{ backgroundColor: '#fff', padding: '30px', borderRadius: '15px', color: '#000', margin: 0, boxShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px', marginBottom: '5vh', justifyContent: 'space-between' }}>
-          <Link to={url} style={{ textDecoration: 'none', color: 'inherit' }}>
-            <div data-aos="fade-up" data-aos-delay="100" className="card" style={{ textAlign: 'center' }}>
-              <h1 style={{fontWeight: 'bold' }}>{title}</h1>
-              <h4>{count}</h4>
-            </div>
-          </Link>
-        </Grid>
+
       </Grid>
     </>
   );
