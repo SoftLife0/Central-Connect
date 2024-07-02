3b5e209f7b030e9a5b7d06c1b21a8b26a165aecd
SoftLife0 <onikosiadewale18@gmail.com>
2024-07-02 04:37:12 +0000

    create a root to render App

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './App.js'
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
